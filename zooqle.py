07f008735cac507626b520e4f9ac23f230d53600
cyberboysumanjay <cyberboysumanjay@gmail.com>
2018-07-13 22:31:15 +0530

    Added Zooqle Module

---
+++ zooqle.py
@@ -0,0 +1,35 @@
+import json
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.parse
+import pyperclip
+import colored
+from colored import stylize
+import re
+
+def zooqle_search(query):
+    scrapper_api='https://api.scraperapi.com/?key=2500866341976677504461594942127656&url='
+    url=scrapper_api+'https://zooqle.unblocked.vet/search?q='+query
+    print("Searching......")
+    source=requests.get(url).text
+    soup=bs(source,'lxml')
+    magnet_results=soup.find_all('a',title='Magnet link',href=True)
+#    print(magnet_results)
+    i=1
+    for a in soup.find_all('a',class_=' small', href=True):
+        print (i," :", a['href'][1:-11])
+        print()
+        i+=1
+
+    index=int(input("Select one from the list below: \n"))
+    m=[]
+    for links in magnet_results:
+        m.append(links['href'])
+    magnet_link=m[index-1]
+#    print(magnet_link)
+    pyperclip.copy(magnet_link)
+    print("Magnet link of the selected movie is copied to clipboard!")
+
+
+
+#zooqle_search(query)
