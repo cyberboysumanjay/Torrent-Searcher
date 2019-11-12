49cea16ee9772900a4f99420944c4c6af4d0029e
Alex <alexcj2802@gmail.com>
2019-11-12 17:42:19 +0000

    Added no download mode, changed site
    Changing site didn't make it work.

--- kickass.py
+++ kickass.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup as bs
 import pyperclip
 import requests
 
-def kickass_search(query):
-    base_url="https://kat.unblocked.gdn/usearch/"
+def kickass_search(query, doDownload):
+    base_url="https://kickasss.to/"
     url=base_url+query+'/'
     print("Searching......")
     source=requests.get(url).text
@@ -14,14 +14,16 @@ def kickass_search(query):
     for r in name:
         print(i,r.text)
         i=i+1
-    print("Enter the Serial Number of the search item you like to download: ")
-    choice=int(input())
-    print ("Fetching data.....")
-    magnet=soup.find_all('a', title="Torrent magnet link")
-    a=[]
-    for m in magnet:
-        a.append(m['href'])
-    magnet_link=(a[choice-1])
-    print("Magnet Link of your selected choice has been fetched.")
-    pyperclip.copy(magnet_link)
-    print ("Your magnet link is now in your clipboard.")
+
+    if doDownload == True:
+        print("Enter the Serial Number of the search item you like to download: ")
+        choice=int(input())
+        print ("Fetching data.....")
+        magnet=soup.find_all('a', title="Torrent magnet link")
+        a=[]
+        for m in magnet:
+            a.append(m['href'])
+        magnet_link=(a[choice-1])
+        print("Magnet Link of your selected choice has been fetched.")
+        pyperclip.copy(magnet_link)
+        print ("Your magnet link is now in your clipboard.")
